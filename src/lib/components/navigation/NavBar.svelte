fbbf5b7d8e16a88e9ca53530946d26f0e6d3118c
Wighawag <wighawag@gmail.com>
2021-04-07 21:58:37 +0100

    loading

--- src/lib/components/navigation/NavBar.svelte
+++ src/lib/components/navigation/NavBar.svelte
@@ -2,11 +2,14 @@
   type LinkInfo = {href: string; title: string};
   export let links: LinkInfo[];
   import NavLink from './NavLink.svelte';
+  import Loading from '../Loading.svelte';
   import {base} from '$app/paths';
-  import {page} from '$app/stores';
+  import {page, navigating} from '$app/stores';
 </script>
 
 <!-- {JSON.stringify($page, null, '  ')} -->
+
+<!-- {JSON.stringify($navigating, null, '  ')} -->
 
 <ul class="flex m-1 border-b border-pink-600">
   {#each links as link}
@@ -19,3 +22,7 @@
     </NavLink>
   {/each}
 </ul>
+
+{#if $navigating}
+  <Loading />
+{/if}
